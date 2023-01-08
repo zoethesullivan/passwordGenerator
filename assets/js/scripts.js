d1b0e28dca1c2396d2e9dbe8cb318da5e9d3144d
zoe_moment <zoetsullivan@gmail.com>
2023-01-07 18:11:06 -0800

    add click funtion to btn el

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,18 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-
+// access btn in dom
 let generatePasswordBtn = document.getElementById ('generate-passwords')
+
+function generatePassword() {
+  console.log('hi')
+}
+generatePasswordBtn.addEventListener("click", generatePassword() )
+
+// click btn 
+
+// click of button changes input for p1
+
+// generate 16 characters in a line
+
+// repeat unique
