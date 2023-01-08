d69acfdafa6c1d3c12d4a65ff0a974e37d712c89
zoe_moment <zoetsullivan@gmail.com>
2023-01-07 18:34:44 -0800

    change text content of password one and two when btn is pushed

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,18 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-// access btn in dom
-let generatePasswordBtn = document.getElementById ('generate-passwords')
+let passwordOne = document.getElementById('password-one')
+let passwordTwo = document.getElementById('password-two')
 
 function generatePassword() {
-  console.log('hi')
+  passwordOne.textContent = "test"
+  passwordTwo.textContent = "testing"
 }
-generatePasswordBtn.addEventListener("click", generatePassword() )
+//  for (let i = 0; i < cards.length; i++) { //cards is the array
+  //  cardsEl.textContent += cards[i] + " " 
+
+document.getElementById ('generate-passwords').addEventListener("click", generatePassword);
+
 
 // click btn 
 
