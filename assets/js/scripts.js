cd15fe4eb8ef84c4e52e1fea4331e666b48fd4e4
zoe_moment <zoetsullivan@gmail.com>
2023-01-08 21:36:42 -0800

    break what little code i have

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,32 +1,14 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-let passwordOne = document.getElementById('password-one')
-let passwordTwo = document.getElementById('password-two')
-let renderPassword = 1
+let randomNumber = Math.floor(Math.random() * characters.length) + 1 ;
 
-function getPassword() {
-  let randomNumber = Math.floor(Math.random() * characters.length) + 1 ;
-  if (renderPassword.length < 16) {
-    renderPassword.textContent += characters[randomNumber]
-  }
+let generatePasswordBtn = document.getElementById('generate-passwords')
+let passwordOneEl = document.getElementById('password-one')
+let passwordTwoEl = document.getElementById('password-two')
+
+passwordFunction(){
+  console.log('hi')
 }
-// function renderPassword() {
-  
-function generatePassword() {
-  passwordOne.textContent = getPassword()
-  passwordTwo.textContent = "testing"
-}
-//  for (let i = 0; i < cards.length; i++) { //cards is the array
-  //  cardsEl.textContent += cards[i] + " " 
 
-document.getElementById ('generate-passwords').addEventListener("click", generatePassword);
-
-
-// click btn 
-
-// click of button changes input for p1
-
-// generate 16 characters in a line
-
-// repeat unique
+generatePasswordBtn.addEventListener('click', passwordFunction())
