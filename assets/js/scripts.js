0e10abc9e232714cefbb412381271eb644e870a9
zoe_moment <zoetsullivan@gmail.com>
2023-01-11 15:59:36 -0800

    move funtion out of click event

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,19 +1,20 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-
-
 let generatePasswordBtn = document.getElementById('generate-passwords')
 let passwordOneEl = document.getElementById('password-one')
 let passwordTwoEl = document.getElementById('password-two')
-let count = 0
+let count = 4
 
 
-generatePasswordBtn.addEventListener("click", function passwordFunction() {
-  for (i=0; i < 17; i++) {
-    let randomNumber = Math.floor(Math.random() * characters.length) + 1 ;
-    let randomPasswordCharacter = characters[randomNumber]
-    passwordOneEl.textContent = randomPasswordCharacter
+function generateRandomPasswordCh(){
+  let randomNumber = Math.floor(Math.random() * characters.length) + 1;
+  let randomPasswordCharacter = characters[randomNumber]
+  return randomPasswordCharacter  
+}
+
+function generateRandomPasswordLength(count) {
+  for(i = 0 ; i < count ; i++){
+  passwordOneEl.textContent = generateRandomPasswordCh()
   }
-  passwordTwoEl.textContent = "ipsum"
-})
+}
