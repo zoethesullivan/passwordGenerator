30bb7a6c02cbdda3d4b34a874791a32931480d1c
zoe_moment <zoetsullivan@gmail.com>
2023-01-11 14:33:00 -0800

    move randomNumber varriable into pasword funtion

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,7 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-let randomNumber = Math.floor(Math.random() * characters.length) + 1 ;
+
 
 let generatePasswordBtn = document.getElementById('generate-passwords')
 let passwordOneEl = document.getElementById('password-one')
@@ -10,7 +10,8 @@ let count = 0
 
 generatePasswordBtn.addEventListener("click", function passwordFunction() {
   for (i=0; i < 17; i++) {
-    console.log(0)
+    let randomNumber = Math.floor(Math.random() * characters.length) + 1 ;
+    console.log(characters[randomNumber])
   passwordOneEl.textContent = "lorem"
 }
   passwordTwoEl.textContent = "ipsum"
