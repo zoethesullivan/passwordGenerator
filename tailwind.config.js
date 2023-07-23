e35d4b80f1ad40286f767fc159e2aa4b0bb8b763
Zoe Sullivan <zoetsullivan@gmail.com>
2023-07-23 13:53:09 -0700

    add config file for tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './assets/js/scripts.js', 
+    './index.html'
+  ],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
